5126c79c984638396e26d398ba4148f4c71f0e53
YUUKI TAMADA <s23c1088ev@s.chibakoudai.jp>
2024-11-07 11:39:06 +0900

    Add test

--- test.bash
+++ test.bash
@@ -1,13 +1,20 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 YUUKI TAMADA
 
 
-ng () {
-	echo ${1}行目が違うよ
-	res=1
-}	
-res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
+### 正常な入力 ###
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+### 異常な入力　###
+out=$(echo あ  | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 
 exit $res
