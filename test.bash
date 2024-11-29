c89cf8bcbe8f7134c209789c5a7aeec739cae58a
YUUKI TAMADA <s23c1088ev@s.chibakoudai.jp>
2024-11-29 20:01:32 +0900

    Add README.md

--- test.bash
+++ test.bash
@@ -1,25 +1,47 @@
-#!/bin/bash -xv
+#!/bin/bash 
 # SPDX-FileCopyrightText: 2024 YUUKI TAMADA
+
 ng () {
-	echo ${1}行目が違うよ
-	res=1
+    echo "${1}行目が違うよ"
+    res=1
 }
+
 res=0
 
 ### 正常な入力 ###
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+# スロット回数を5回で実行した場合の出力を確認
+out=$(echo 5 | python3 slot)
+if [[ ! "${out}" =~ "回数: 5回" ]]; then
+    ng "$LINENO"
+fi
 
-### 異常な入力　###
-out=$(echo あ  | ./plus)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+# 結果が表示されることを確認（3つの絵文字が出力される）
+if [[ ! "${out}" =~ "🍒" && ! "${out}" =~ "🍋" && ! "${out}" =~ "🍊" && ! "${out}" =~ "🔔" && ! "${out}" =~ "７" && ! "${out}" =~ "💎" ]]; then
+    ng "$LINENO"
+fi
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]  || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+### 異常な入力 ###
+
+# 1~10以外の数字を入力した場合（例えば11）
+out=$(echo 11 | python3 slot)
+if [[ "$?" -eq 0 ]]; then
+    ng "$LINENO"
+fi
+
+# 無効な入力（例えば文字列）
+out=$(echo あ | python3 slot)
+if [[ "$?" -eq 0 ]]; then
+    ng "$LINENO"
+fi
+
+# 入力なし
+out=$(echo | python3 slot)
+if [[ "$?" -eq 0 ]]; then
+    ng "$LINENO"
+fi
 
 [ "$res" = 0 ] && echo OK
 
 exit $res
+
